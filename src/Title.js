12980d42a3e2056b44988f1c8e7b5b059642b9a3
Grayson faircloth <graysonfaircloth@Graysons-MacBook-Pro.local>
2017-05-22 14:09:47 -0400

    Add Title, Header, App components into React App

---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './CSS/Title.css';
+
+
+class Title extends Component {
+    render() {
+        return (
+            <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
+        )
+    }
+}
