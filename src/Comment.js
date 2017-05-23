4401bdaef6d0a83748c3349f36e5eec6d77a7b03
Grayson faircloth <graysonfaircloth@Graysons-MacBook-Pro.local>
2017-05-23 10:23:42 -0400

    Make comments show up below comment box

--- src/Comment.js
+++ src/Comment.js
@@ -2,13 +2,48 @@ import React, { Component } from 'react'
 import './CSS/Comment.css'
 
 class Comment extends Component {
-    
+    constructor(props) {
+        super(props)
+        this.state = {
+            comment: '',
+            comments: []
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.addComment = this.addComment.bind(this)
+    }
+    handleChange(ev) {
+        this.setState({
+            comment: ev.targetValue
+        })
+    }
+    addComment(ev) {
+        
+        const state = {...this.state}
+        const comment = {
+            text: state.comment,
+            time: new Date()
+        }
+        state.comments.push(comment)
+        state.comment = ''
+        this.setState(state)
+
+    }
+
     render() {
         return (
-            <form hidden = {this.props.clicked}>
-                <input type="text"/>
-                <button type="submit"> Comment </button>
+            <div>
+            <form hidden = {this.props.clicked} onSubmit = {this.addComment}>
+                <input type="text" value = {this.state.comment} onChange = {this.handleChange}/>
+                <button type="submit" className = "button"> Comment </button>
+                {this.state.comments.map((comment, ind) => {
+                    return (
+                        <div className = "comment" key = {ind}>
+                            <div> {comment.text} </div>
+                        </div>
+                    )})}
+                
             </form>
+            </div>
         )
     }
 }
